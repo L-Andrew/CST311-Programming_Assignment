a81f64a8b6ed3fafbaba9c19edb85222556e389f
L-Andrew <chunyenl+github@gmail.com>
2018-10-15 15:48:07 -0700

    .

---
+++ tcp/TCPServer.py
@@ -0,0 +1,23 @@
+# UDPPingerServer.py
+# We will need the following module to generate randomized lost packets
+from socket import *
+serverSocket = socket(AF_INET, SOCK_STREAM)
+# Assign IP address and port number to socket
+serverSocket.bind(('', 12000))
+serverSocket.listen(1)
+print('Server ready')
+while True:
+	# Receive the client packet along with the address it is coming from
+	connectionSocketA, addressA = serverSocket.accept()
+	messageA = connectionSocketA.recv(1024).decode()
+	print(messageA)
+	connectionSocketB, addressB = serverSocket.accept()
+	messageB = connectionSocketB.recv(1024).decode()
+	print(messageB)
+	# Capitalize the message from the client
+	messageA = messageA.upper()
+	# Otherwise, the server responds
+	connectionSocketA.sendto(messageA.encode())
+	connectionSocketA.close()
+	connectionSocketB.sendto(messageA.encode())
+	connectionSocketB.close()
