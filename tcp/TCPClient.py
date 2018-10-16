719dd403b6a84026ec5d4a122072095cdda65a2a
evgonzales <evang0598@aol.com>
2018-10-16 16:05:59 -0700

    Error handling and logging format

--- tcp/TCPClient.py
+++ tcp/TCPClient.py
@@ -3,19 +3,23 @@ import random, string
 
 dest = ("localhost", 12000)
 
-c_socket = socket(AF_INET, SOCK_STREAM)
-c_socket.connect(dest)
+try:
+	c_socket = socket(AF_INET, SOCK_STREAM)
+	c_socket.connect(dest)
+except:
+	print(f"[!] Failed to connect to server {dest[0]}:{dest[1]}")
+	exit()
 
 clientID = ''.join(random.choice(string.ascii_uppercase) for i in range(8))
 
 print(f"[!] Connected to {dest[0]}:{dest[1]}")
 print(f"[.] Using ID: {clientID}")
 
-sentence = input("... Input message: ")
+sentence = input("[?] Input message: ")
 message = f"Client ID {clientID}: {sentence}"
 print(f"[.] Sending \"{message}\"")
 c_socket.send(message.encode())
 
 modifiedSentence = c_socket.recv(1024)
-print (f"[!] Received from server: \n\t{modifiedSentence.decode()}")
+print(f"[!] Received from server: \n\t{modifiedSentence.decode()}")
 c_socket.close()
