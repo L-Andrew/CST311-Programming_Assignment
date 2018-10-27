6cdcd8f960c180f667fe99dd372c022231a7b92a
evgonzales <43556667+evgonzales@users.noreply.github.com>
2018-10-26 21:50:27 -0700

    Reworking TCPClient and TCPServer (heavily; moved to V2)

--- tcp/TCPClient.py
+++ tcp/TCPClient.py
@@ -1,25 +1,81 @@
 from socket import *
-import random, string
+from select import select
+from threading import Thread
+import random, string, sys
 
-dest = ("localhost", 12000)
+HOST_ADDRESS = "localhost"
+HOST_PORT = 12000
 
-try:
-	c_socket = socket(AF_INET, SOCK_STREAM)
-	c_socket.connect(dest)
-except:
-	print(f"[!] Failed to connect to server {dest[0]}:{dest[1]}")
-	exit()
+MSG_BUFFER_SIZE = 1024
 
-clientID = ''.join(random.choice(string.ascii_uppercase) for i in range(8))
+CONNECTING_STR_FMT = "[.] Generating ID and connecting to {}:{}..."
+FAILED_TO_CONNECT_STR_FMT = "[!] Failed to connect to server {}:{}"
+SUCCESS_CONNECT_STR_FMT = "[.] Connected, sending name..."
+FAILED_TO_SEND_NAME_STR_FMT = "[!] Failed to send our name ({})!"
+UNKNOWN_ERROR_STR_FMT = "[!] Unknown error!"
 
-print(f"[!] Connected to {dest[0]}:{dest[1]}")
-print(f"[.] Using ID: {clientID}")
+def listen(client_socket, name):
+	while True:
+		try:
+			message = client_socket.recv(MSG_BUFFER_SIZE)
+			print(message.decode().replace(name, name + " (You)"))
+		except:
+			print("[!] Connection closed.")
+			break
+			
+def generateClientID():
+	return ''.join(random.choice(string.ascii_uppercase) for i in range(8))
 
-sentence = input("[?] Input message: ")
-message = f"Client ID {clientID}: {sentence}"
-print(f"[.] Sending \"{message}\"")
-c_socket.send(message.encode())
+def connect(hostname, port):
+	try:
+		client_socket = socket(AF_INET, SOCK_STREAM)
+		client_socket.connect((hostname, port))
+		return client_socket
+	except:
+		return None
+	
+if __name__ == "__main__":
+	# connect to server
+	print(CONNECTING_STR_FMT.format(HOST_ADDRESS, HOST_PORT))
+	
+	client_socket = connect(HOST_ADDRESS, HOST_PORT)
+	name = generateClientID()
+	
+	if client_socket is None:
+		print(FAILED_TO_CONNECT_STR_FMT.format(HOST_ADDRESS, HOST_PORT))
+		sys.exit(1)
+		
+	print(SUCCESS_CONNECT_STR_FMT)
+	
+	try:
+		# send our client name
+		client_socket
+		client_socket.send(name.encode())
+	except Exception as exc:
+		print(exc)
+		print(FAILED_TO_SEND_NAME_STR_FMT.format(name))
+		sys.exit(1)
 
-modifiedSentence = c_socket.recv(1024)
-print(f"[!] Received from server: \n\t{modifiedSentence.decode()}")
-c_socket.close()
+	# unfortunately, due Windows's lack of file descriptors, 
+	# we must spawn a seperate thread to read stdin
+	# otherwise, on *nix systems, we would use select()
+	recv_and_print_thread = Thread(target=listen, args=(client_socket,name))
+	recv_and_print_thread.daemon = True
+	recv_and_print_thread.start()
+	
+	while True:
+		try:
+			message = input()
+			if not recv_and_print_thread.isAlive():
+				# interpret isAlive() being false as connection being closed
+				sys.exit(0)
+			client_socket.send(message.encode())
+			if message.lower() == "bye":
+				client_socket.close()
+				raise KeyboardInterrupt
+		except KeyboardInterrupt:
+			sys.exit(0)
+		except Exception as exc:
+			print(UNKNOWN_ERROR_STR_FMT)
+			print(exc)
+			sys.exit(1)
