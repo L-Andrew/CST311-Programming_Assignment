3c1e4fd8f92fd8190f177d31f5ef740ceb5381d8
evgonzales <43556667+evgonzales@users.noreply.github.com>
2018-10-28 15:43:53 -0700

    Added first user logic to server + docs

--- tcp/TCPClient.py
+++ tcp/TCPClient.py
@@ -3,11 +3,14 @@ from select import select
 from threading import Thread
 import random, string, sys
 
+# change to whatever necessary
 HOST_ADDRESS = "localhost"
 HOST_PORT = 12000
 
+# recv buffer size
 MSG_BUFFER_SIZE = 1024
 
+# MESSAGE FORMATS: These are not used for chat messages; they're purely status messages.
 CONNECTING_STR_FMT = "[.] Generating ID and connecting to {}:{}..."
 FAILED_TO_CONNECT_STR_FMT = "[!] Failed to connect to server {}:{}"
 SUCCESS_CONNECT_STR_FMT = "[.] Connected, sending name..."
@@ -15,6 +18,10 @@ FAILED_TO_SEND_NAME_STR_FMT = "[!] Failed to send our name ({})!"
 UNKNOWN_ERROR_STR_FMT = "[!] Unknown error!"
 
 def listen(client_socket, name):
+	'''
+	Main loop for receving and outputting data from the server.
+	Automatically closes if error occurs.
+	'''
 	while True:
 		try:
 			message = client_socket.recv(MSG_BUFFER_SIZE)
@@ -24,9 +31,16 @@ def listen(client_socket, name):
 			break
 			
 def generateClientID():
+	'''
+	Generates a random 8 character long ID.
+	'''
 	return ''.join(random.choice(string.ascii_uppercase) for i in range(8))
 
 def connect(hostname, port):
+	'''
+	Creates a socket and connects to the server.
+	Returns the socket or None if it failed.
+	'''
 	try:
 		client_socket = socket(AF_INET, SOCK_STREAM)
 		client_socket.connect((hostname, port))
@@ -41,6 +55,7 @@ if __name__ == "__main__":
 	client_socket = connect(HOST_ADDRESS, HOST_PORT)
 	name = generateClientID()
 	
+	# check for client_socket being None, indicating failure
 	if client_socket is None:
 		print(FAILED_TO_CONNECT_STR_FMT.format(HOST_ADDRESS, HOST_PORT))
 		sys.exit(1)
@@ -65,15 +80,18 @@ if __name__ == "__main__":
 	
 	while True:
 		try:
+			# grab user input
 			message = input()
 			if not recv_and_print_thread.isAlive():
 				# interpret isAlive() being false as connection being closed
 				sys.exit(0)
+			# send the data over to the server
 			client_socket.send(message.encode())
+			# after sent, check if message is "bye", and if it is, then interpret as ctrl+c
 			if message.lower() == "bye":
-				client_socket.close()
 				raise KeyboardInterrupt
 		except KeyboardInterrupt:
+			client_socket.close()
 			sys.exit(0)
 		except Exception as exc:
 			print(UNKNOWN_ERROR_STR_FMT)
--- tcp/TCPServer.py
+++ tcp/TCPServer.py
@@ -1,23 +1,155 @@
-# UDPPingerServer.py
-# We will need the following module to generate randomized lost packets
 from socket import *
-serverSocket = socket(AF_INET, SOCK_STREAM)
-# Assign IP address and port number to socket
-serverSocket.bind(('', 12000))
-serverSocket.listen(1)
-print('Server ready')
-while True:
-	# Receive the client packet along with the address it is coming from
-	connectionSocketA, addressA = serverSocket.accept()
-	messageA = connectionSocketA.recv(1024).decode()
-	print(messageA)
-	connectionSocketB, addressB = serverSocket.accept()
-	messageB = connectionSocketB.recv(1024).decode()
-	print(messageB)
-	# Capitalize the message from the client
-	messageA += ' received before ' + messageB
-	# Otherwise, the server responds
-	connectionSocketA.send(messageA.encode())
-	connectionSocketA.close()
-	connectionSocketB.send(messageA.encode())
-	connectionSocketB.close()
+from select import select
+from threading import Thread
+import sys, threading, time
+
+# CONFIG: Change these to whatever necessary
+BINDING_ADDRESS = '0.0.0.0'
+BINDING_PORT = 12000
+MSG_BUFFER_SIZE = 1024
+NAME_BUFFER_SIZE = 32
+
+# MESSAGE FORMATS: Change these to suit a flavor of console.
+# Technically speaking, these also effect clients.
+# (Clients do not print their own message - they wait for the server to respond with it)
+NEW_CONNECTION_STR_FMT = "[.] Accepted new connection from {}."
+NEW_USER_FIRST_STR_FMT = "[!] {} has joined"
+NEW_USER_NOT_FIRST_STR_FMT = "[!] {} has joined (First user was {})"
+NEW_MSG_STR_FMT = "[.] {}: {}"
+DISCONNECTED_STR_FMT = "[!] {} has disconnected."
+CLOSING_STR_FMT = "[!] Server is closing."
+
+class ChatServer:
+	def __init__(self):
+		'''
+		Constructs a new ChatServer instance.
+		Note: The server is _not_ listening. You must call startHosting(hostname, port).
+		'''
+		self.host_socket = None
+		self.connected = {}
+		self.first = None
+	
+	def startHosting(self, hostname, port):
+		'''
+		Creates a TCP socket and binds it to the given hostname and port.
+		'''
+		self.host_socket = socket(AF_INET, SOCK_STREAM)
+		self.host_socket.bind((BINDING_ADDRESS, BINDING_PORT))
+		self.host_socket.listen(1)
+		
+	def acceptNewUser(self):
+		'''
+		Accepts and stores any new connections, waiting for their names.
+		'''
+		new_client, addr = self.host_socket.accept()
+		self.connected[new_client] = None
+		print(NEW_CONNECTION_STR_FMT.format(addr))
+		
+	def removeDeadSocket(self, socket):
+		'''
+		Removes any socket from the name mapping.
+		Assumes the socket is dead and will notify users of their disconnection.
+		'''
+		name = self.connected[socket]
+		self.connected.pop(socket)
+		if name != None:
+			self.broadcastMessage(DISCONNECTED_STR_FMT, name)
+		
+	def writeData(self, socket, data):
+		'''
+		Attempts to write data to the socket, removing it if dead
+		'''
+		try:
+			socket.send(data)
+		except ConnectionResetError:
+			self.removeDeadSocket(socket)
+			
+	def readData(self, socket, buffer_size):
+		'''
+		Attempts to read string data from a socket, removing it if dead.
+		'''
+		try:
+			data = socket.recv(buffer_size)
+			
+			if len(data) == 0:
+				self.removeDeadSocket(socket)
+				return None
+				
+			return data.decode()
+		except ConnectionResetError:
+			self.removeDeadSocket(socket)
+			return None
+		
+	def broadcastMessage(self, fmt, *args):
+		'''
+		Broadcasts a message to all clients and prints to console.
+		'''
+		msg = fmt.format(*args)
+		print(msg)
+		for socket in self.connected.keys():
+			self.writeData(socket, msg.encode())
+		
+	def tick(self):
+		'''
+		Main server ticking method.
+		Would have been single threaded, but alas, Windows select() does _not_ handle sys.stdin.
+		Therefore, it is necessary to spin this in a different thread, as select() _will_ lock
+		the process up until some socket event happens.
+		'''
+		active_sockets, _, _ = select([self.host_socket] + list(self.connected.keys()), [], [])
+		
+		for socket in active_sockets:
+			if socket == self.host_socket:
+				self.acceptNewUser()
+			else:
+				if socket not in self.connected:
+					print("[?] Unknown socket")
+					print(socket)
+				if self.connected[socket] == None:
+					name = self.readData(socket, NAME_BUFFER_SIZE)
+					if name is not None:
+						self.connected[socket] = name
+						if self.first == None:
+							self.first = name
+							self.broadcastMessage(NEW_USER_FIRST_STR_FMT, name)
+						else:
+							self.broadcastMessage(NEW_USER_NOT_FIRST_STR_FMT, name, self.first)
+				else:
+					message = self.readData(socket, MSG_BUFFER_SIZE)
+					if message is not None:
+						self.broadcastMessage(NEW_MSG_STR_FMT, self.connected[socket], message)
+	
+	def close(self):
+		'''
+		Does what it says on the tin - closes the server.
+		'''
+		self.broadcastMessage(CLOSING_STR_FMT)
+		self.host_socket.close()
+
+# dummy method because python doesn't support
+# multi-line lambdas
+def thread_loop(server):
+	while True:
+		server.tick()
+
+if __name__ == "__main__":
+	# start server and begin listening
+	server = ChatServer()
+	server.startHosting(BINDING_ADDRESS, BINDING_PORT)
+	
+	# spin server ticks on a seperate thread to let main thread process ctrl+c
+	# and user input
+	server_thread = Thread(target=thread_loop, args=(server,))
+	server_thread.daemon = True
+	server_thread.start()
+	
+	while True:
+		try:
+			server_input = input()
+			if server_input.lower() == "bye":
+				# bad hack, but don't want to repeat self
+				# tl;dr just interpret "bye" as ctrl+c
+				raise KeyboardInterrupt
+		except KeyboardInterrupt:
+			server.close()
+			sys.exit(0)
